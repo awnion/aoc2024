277fd938ff7ece9a2d7e60a599b9e6dce64fe5c2
Sergei Blinov <blinovsv@gmail.com>
2024-12-02 10:31:23 +0100

    add rustfmt

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,14 @@
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelBridge;
+use rayon::iter::ParallelIterator;
 
 pub fn read_input(input: &str) -> (Vec<i128>, Vec<i128>) {
-    let nums = input
-        .split_ascii_whitespace()
-        .filter_map(|s| s.parse().ok())
-        .collect::<Vec<_>>();
+    let nums = input.split_ascii_whitespace().filter_map(|s| s.parse().ok()).collect::<Vec<_>>();
 
     (
         nums.iter().skip(0).step_by(2).cloned().collect(),
@@ -15,62 +19,45 @@ pub fn read_input(input: &str) -> (Vec<i128>, Vec<i128>) {
 pub fn part1_solution(input: &str) -> u128 {
     let (list1, list2) = {
         let (mut a, mut b) = read_input(input);
-        a.sort();
-        b.sort();
+        a.sort_unstable();
+        b.sort_unstable();
         (a, b)
     };
 
-    list1.iter().zip(&list2).map(|(&x, &y)| x.abs_diff(y)).sum()
+    list1.iter().zip(&list2).par_bridge().into_par_iter().map(|(&x, &y)| x.abs_diff(y)).sum()
 }
 
 pub fn part2_solution(input: &str) -> i128 {
     let (list1, list2) = read_input(input);
 
-    let mut counter = HashMap::new();
+    let counter = Arc::new(Mutex::new(HashMap::new()));
 
-    for item in list2 {
-        counter.entry(item).and_modify(|v| *v += 1).or_insert(1);
-    }
+    list2.par_iter().for_each(|&item| {
+        counter.lock().unwrap().entry(item).and_modify(|v| *v += 1).or_insert(1);
+    });
 
-    list1
-        .iter()
-        .map(|item| item * counter.get(item).unwrap_or(&0))
-        .sum()
+    list1.par_iter().map(|item| item * counter.lock().unwrap().get(item).unwrap_or(&0)).sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_test() {
-        assert_eq!(
-            part1_solution(
-                "\
+    const INPUT: &'static str = "\
 3   4
 4   3
 2   5
 1   3
 3   9
-3   3"
-            ),
-            11u128
-        );
+3   3";
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(part1_solution(INPUT), 11u128);
     }
 
     #[test]
     fn part2_test() {
-        assert_eq!(
-            part2_solution(
-                "\
-3   4
-4   3
-2   5
-1   3
-3   9
-3   3"
-            ),
-            31i128
-        );
+        assert_eq!(part2_solution(INPUT), 31i128);
     }
 }
